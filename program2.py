a50b95204e1aca4790fe28d237d735fcc119616a
Bathinenipranathi <140799650+Bathinenipranathi@users.noreply.github.com>
2023-07-28 16:09:36 +0530

    Update program2.py

--- program2.py
+++ program2.py
@@ -20,8 +20,6 @@ else:
 
 
 # In[19]:
-
-
 def bin2Dec(val):
     rev=val[::-1]
     dec = 0
@@ -38,25 +36,27 @@ def oct2Hex(val):
     for dig in rev:
          dec += int(dig) * 8**i
          i += 1
-list=[]
-while dec != 0:
-    list.append(dec%16)
-    dec = dec // 16
+    list=[]
+    while dec!= 0:
+            list.append(dec%16)
+            dec = dec // 16
 
 
-nl=[]
-for elem in list[::-1]:
-    if elem <= 9:
-        nl.append(str(elem))
-    else:
-        nl.append(chr(ord('A') + (elem -10)))
-hex = "".join(nl)
+    nl=[]
+    for elem in list[::-1]:
+        if elem <= 9:
+            nl.append(str(elem))
+        else:
+            nl.append(chr(ord('A') + (elem -10)))
+    hex = "".join(nl)
 
-return hex
+    return hex
 num1 = input("Enter a binary number : ")
 print(bin2Dec(num1))
 num2 = input("Enter a octal number : ")
 print(oct2Hex(num2))
+
+
 
 
 # In[ ]:
