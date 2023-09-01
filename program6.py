462c1cd061b881e5f210f07cccdc5b86cb6caf42
Bathinenipranathi <140799650+Bathinenipranathi@users.noreply.github.com>
2023-09-01 15:30:08 +0530

    Add files via upload

---
+++ program6.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+import os.path
+import sys
+fname = input("Enter the filename : ") 
+if not os.path.isfile(fname):
+    print("File", fname, "doesn't exists")
+    sys.exit(0)
+infile = open(fname, "r")
+lineList = infile.readlines()
+for i in range(4):
+    print(i+1, ":",lineList[i])
+ 
+word = input("Enter a word : ")
+cnt = 0
+for line in lineList:
+    cnt += line.count(word)
+print("The word", word, "appears", cnt, "times in the file")
+
+
+# In[6]:
+
+
+import os
+import sys
+import pathlib
+import zipfile
+dirName = input("Enter Directory name that you want to backup : ")
+if not os.path.isdir(dirName):
+    print("Directory", dirName, "doesn't exists")
+    sys.exit(0)
+ 
+curDirectory = pathlib.Path(dirName)
+ 
+with zipfile.ZipFile("myZip.zip", mode="w") as archive:
+    for file_path in curDirectory.rglob("*"):
+        archive.write(file_path, arcname=file_path.relative_to(curDirectory))
+ 
+if os.path.isfile("myZip.zip"):
+    print("Archive", "myZip.zip", "created successfully")
+else:
+    print("Error in creating zip archive")
+
+
+# In[ ]:
+
+
+
+
